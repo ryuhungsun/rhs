e36be988f9be8e4442f363be6ee4aa097a7c84d9
topmajor <topmajor@naver.com>
2020-09-28 18:03:20 +0900

    최초

---
+++ src/main/java/com/rhs/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.rhs;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DevProjectApplication.class);
+	}
+
+}
